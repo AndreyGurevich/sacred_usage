bfc14ca00a119ebe199ef9743d2bbdf0165e11b8
Andrey Gurevich <amber4eg@gmail.com>
2020-02-23 16:10:21 +0300

    sacred without observer

--- iris_example.py
+++ iris_example.py
@@ -1,21 +1,21 @@
 from numpy.random import permutation
 from sklearn import svm, datasets
+from sacred import Experiment
+ex = Experiment('iris_rbf_svm')
 
+@ex.config
+def cfg():
+  C = 1.0
+  gamma = 0.7
 
-
-
-
-C = 1.0
-gamma = 0.7
-
-
-
-iris = datasets.load_iris()
-perm = permutation(iris.target.size)
-iris.data = iris.data[perm]
-iris.target = iris.target[perm]
-clf = svm.SVC(C, 'rbf', gamma=gamma)
-clf.fit(iris.data[:90],
-        iris.target[:90])
-print(clf.score(iris.data[90:],
-                iris.target[90:]))
+@ex.automain
+def run(C, gamma):
+  iris = datasets.load_iris()
+  per = permutation(iris.target.size)
+  iris.data = iris.data[per]
+  iris.target = iris.target[per]
+  clf = svm.SVC(C, 'rbf', gamma=gamma)
+  clf.fit(iris.data[:90],
+          iris.target[:90])
+  return clf.score(iris.data[90:],
+                   iris.target[90:])
