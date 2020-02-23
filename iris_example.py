9fd8da3f1eb8a1ebc87f1d227620898a6d88d285
Andrey Gurevich <amber4eg@gmail.com>
2020-02-23 16:32:33 +0300

    sacred with observer

--- iris_example.py
+++ iris_example.py
@@ -1,7 +1,15 @@
 from numpy.random import permutation
 from sklearn import svm, datasets
 from sacred import Experiment
+from sacred.observers import MongoObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+
+observer = MongoObserver(
+    url='mongodb://user:pass@host/omniboard?authMechanism=SCRAM-SHA-256',
+    db_name='omniboard')
 ex = Experiment('iris_rbf_svm')
+ex.observers.append(observer)
+ex.captured_out_filter = apply_backspaces_and_linefeeds
 
 @ex.config
 def cfg():
@@ -11,11 +19,12 @@ def cfg():
 @ex.automain
 def run(C, gamma):
   iris = datasets.load_iris()
-  per = permutation(iris.target.size)
-  iris.data = iris.data[per]
-  iris.target = iris.target[per]
+  perm = permutation(iris.target.size)
+  iris.data = iris.data[perm]
+  iris.target = iris.target[perm]
   clf = svm.SVC(C, 'rbf', gamma=gamma)
   clf.fit(iris.data[:90],
           iris.target[:90])
-  return clf.score(iris.data[90:],
-                   iris.target[90:])
+  ex.log_scalar("Some metric", 0.85)
+  return float(clf.score(iris.data[90:],
+                         iris.target[90:]))
